e06d9ab3bac7f7e7e58e55d6fc3c867f9d7ad5e4
andremartingo <andre.martingo@gmail.com>
2018-06-11 21:41:52 +0100

    create inital view controller inside navigation controller by code

--- appstore/AppDelegate.swift
+++ appstore/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewLayout()
+        let featuredAppsController = FeaturedAppsController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController: featuredAppsController)
         // Override point for customization after application launch.
         return true
     }
