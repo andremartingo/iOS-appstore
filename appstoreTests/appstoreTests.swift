c3cd2df47665df7babb8c1cce961aa305b51c540
andremartingo <andre.martingo@gmail.com>
2018-06-11 21:35:35 +0100

    inital commit

---
+++ appstoreTests/appstoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  appstoreTests.swift
+//  appstoreTests
+//
+//  Created by André Martingo on 11/06/2018.
+//  Copyright © 2018 André Martingo. All rights reserved.
+//
+
+import XCTest
+@testable import appstore
+
+class appstoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
