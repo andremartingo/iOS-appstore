a8f69c0377ce7a223fe3e19d21bdc2fe50628857
andremartingo <andre.martingo@gmail.com>
2018-06-12 09:19:03 +0100

    create custom view cell inside the new collection view

--- appstore/CategoryCell.swift
+++ appstore/CategoryCell.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class CategoryCell: UICollectionViewCell{
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    private let appCellId = "appCellId"
     
     override init(frame: CGRect){
         super.init(frame: frame)
@@ -19,9 +21,12 @@ class CategoryCell: UICollectionViewCell{
         fatalError("init(coder:) has not been implemented")
     }
     
+    //Collection View Inside Cell
     let appsCollectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
         collectionView.backgroundColor = UIColor.blue
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
@@ -29,8 +34,47 @@ class CategoryCell: UICollectionViewCell{
     
     func setupViews(){
         backgroundColor = UIColor.black
+        
         addSubview(appsCollectionView)
+        appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: appCellId)
+        
+        appsCollectionView.delegate = self
+        appsCollectionView.dataSource = self
+        
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: appCellId, for: indexPath) as! AppCell
+        return cell
+    }
 }
+
+class AppCell: UICollectionViewCell {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        backgroundColor = UIColor.red
+    }
+    
+}
+
+
+
+
+
+
+
+
