36a3ed5d1a7eb52d733cf0954cc543bd152294c5
andremartingo <andre.martingo@gmail.com>
2018-06-13 09:11:33 +0100

    create nameLabel att AppCell

--- appstore/CategoryCell.swift
+++ appstore/CategoryCell.swift
@@ -71,7 +71,7 @@ class AppCell: UICollectionViewCell {
         setupViews()
     }
     
-    let imageView: UIImageView = {
+    let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "frozen")
         imageView.contentMode = .scaleAspectFill
@@ -80,13 +80,23 @@ class AppCell: UICollectionViewCell {
         return imageView
     }()
     
+    let nameLabel: UILabel = {
+        let label =  UILabel()
+        label.text = "Disney Build It: Frozen"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 2
+        return label
+    }()
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func setupViews(){
-        addSubview(imageView)
-        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        addSubview(logoImageView)
+        addSubview(nameLabel)
+        logoImageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        nameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
     }
     
 }
