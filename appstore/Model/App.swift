5fe262c14230532a53aef7486b4b9a0e2a36a7b6
andremartingo <andre.martingo@gmail.com>
2018-06-18 09:10:23 +0100

    create App and Category Model

---
+++ appstore/Model/App.swift
@@ -0,0 +1,18 @@
+//
+//  App.swift
+//  appstore
+//
+//  Created by André Martingo on 18/06/2018.
+//  Copyright © 2018 André Martingo. All rights reserved.
+//
+
+import UIKit
+
+class App: NSObject {
+    
+    var id: NSNumber?
+    var name: String?
+    var category: String?
+    var imageName: String?
+    var price: NSNumber?
+}
---
+++ appstore/Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  appstore
+//
+//  Created by André Martingo on 18/06/2018.
+//  Copyright © 2018 André Martingo. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+    
+    var name: String?
+    var apps: [App]?
+}
+
