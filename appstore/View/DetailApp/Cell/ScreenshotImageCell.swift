a71a52c58f0d222ef600e69b763015b42cef909c
andremartingo <andre.martingo@gmail.com>
2018-06-27 13:32:51 +0100

    add imageView to screenshotImage Cell

--- appstore/View/DetailApp/Cell/ScreenshotImageCell.swift
+++ appstore/View/DetailApp/Cell/ScreenshotImageCell.swift
@@ -21,12 +21,29 @@ class ScreenshotImageCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        setupConstraints()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    let screenshotImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = UIColor.green
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
     func setupViews() {
+        addSubview(screenshotImageView)
+    }
+
+    func setupConstraints() {
+        screenshotImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        screenshotImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
+        screenshotImageView.heightAnchor.constraint(equalToConstant: frame.height).isActive = true
+        screenshotImageView.widthAnchor.constraint(equalToConstant: frame.width).isActive = true
     }
 }
