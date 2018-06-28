0568e36ffddabf000eff82dabdfea5287d6f55f3
andremartingo <andre.martingo@gmail.com>
2018-06-28 19:43:49 +0100

    remove warning from collectionView

--- appstore/View/DetailApp/DetailAppSource.swift
+++ appstore/View/DetailApp/DetailAppSource.swift
@@ -2,12 +2,12 @@ import UIKit
 import Foundation
 
 class DetailAppSource: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
+    
     let collectionView: UICollectionView
-
+    
     private let detailHeaderId = "detailHeaderId"
     private let screenshotId = "screenshotId"
-
+    
     var app: App {
         didSet {
             performUIUpdatesOnMain {
@@ -15,7 +15,7 @@ class DetailAppSource: NSObject, UICollectionViewDelegate, UICollectionViewDataS
             }
         }
     }
-
+    
     init(collection: UICollectionView, app: App) {
         self.collectionView = collection
         self.app = app
@@ -24,41 +24,43 @@ class DetailAppSource: NSObject, UICollectionViewDelegate, UICollectionViewDataS
         self.collectionView.dataSource = self
         self.collectionView.register(DetailHeaderCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: detailHeaderId)
         self.collectionView.register(ScreenshotsCell.self, forCellWithReuseIdentifier: screenshotId)
-
+        
     }
-
+    
     // MARK: HEADER
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
         guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: detailHeaderId, for: indexPath) as? DetailHeaderCell else {
             return UICollectionReusableView()
         }
         header.app = app
         return header
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 170)
     }
-
+    
     // MARK: BODY
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 1
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: screenshotId, for: indexPath) as? ScreenshotsCell else {
             return UICollectionViewCell()
         }
-//        guard let image = app.screenshots else {
-//            return UICollectionViewCell()
-//        }
-//        cell.images = image
+        //        guard let image = app.screenshots else {
+        //            return UICollectionViewCell()
+        //        }
+        //        cell.images = image
         if let image = app.screenshots {
             cell.images = image
         }
         return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 200)
     }
