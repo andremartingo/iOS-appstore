cfb82fc4c9e816d16b599a3aba605b39ac9d9128
andremartingo <andre.martingo@gmail.com>
2018-06-28 09:22:34 +0100

    change set view to loadView instead viewDidLoad

--- appstore/Controller/DetailAppController.swift
+++ appstore/Controller/DetailAppController.swift
@@ -13,6 +13,7 @@ class DetailAppController: UIViewController {
     private let detailHeaderId = "detailHeaderId"
     var mainView: DetailAppView!
     var mainSource: DetailAppSource?
+    var store: APIRepository?
 
     var app: App? {
         didSet {
@@ -20,10 +21,26 @@ class DetailAppController: UIViewController {
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
         mainView = DetailAppView(frame: UIScreen.main.bounds)
         view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         self.mainSource = DetailAppSource(collection: mainView.appsCollectionView, app: app!)
+        self.store = APIRepository()
+        if let appId = app?.id {
+            self.getAppById(appId: appId)
+        }
+    }
+
+    func getAppById(appId: NSNumber) {
+        store?.getAppById(appId: appId, { (appDetail, _) in
+            self.app = appDetail
+            performUIUpdatesOnMain {
+                self.mainView.appsCollectionView.reloadData()
+            }
+        })
     }
 }
