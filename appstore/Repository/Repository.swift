6e589a1058145e1ba7b18ad3370ec48f471a9270
andremartingo <andre.martingo@gmail.com>
2018-06-28 19:38:52 +0100

    implement TypeAlias

--- appstore/Repository/Repository.swift
+++ appstore/Repository/Repository.swift
@@ -1,6 +1,8 @@
 protocol Repository {
 
-    func getFeaturedCategories(completionHandler: @escaping (_ result: FeatureApp?, _ error: Error?) -> Void)
+    typealias FeatureCategoriesHandler = (_ result: FeatureApp?, _ error: Error?) -> Void
+
+    func getFeaturedCategories(completionHandler: @escaping FeatureCategoriesHandler)
 
     func getAppById(appId: Int, _ completionHandlergetFeaturedCategories: @escaping (_ result: App?, _ error: Error?) -> Void)
 }
