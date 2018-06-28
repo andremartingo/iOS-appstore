9068ba9c14e7c94c866fbe53e35c0bd991584575
andremartingo <andre.martingo@gmail.com>
2018-06-28 09:23:25 +0100

    refactor repository protocol to have completion handler

--- appstore/Repository/Repository.swift
+++ appstore/Repository/Repository.swift
@@ -5,7 +5,10 @@
 //  Created by André Martingo on 18/06/2018.
 //  Copyright © 2018 André Martingo. All rights reserved.
 //
-
+import Foundation
 protocol Repository {
-    func getFeaturedCategories() -> FeatureApp
+
+    func getFeaturedCategories(_ completionHandlergetFeaturedCategories: @escaping (_ result: FeatureApp?, _ error: NSError?) -> Void)
+
+      func getAppById(appId: NSNumber, _ completionHandlergetFeaturedCategories: @escaping (_ result: App?, _ error: NSError?) -> Void)
 }
